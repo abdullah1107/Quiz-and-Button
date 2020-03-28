84e27965027f42152cac61939fa6ab0a5c99edbb
abdullah1107 <abdullah.cse11@gmail.com>
2020-03-28 15:57:49 +0600

    button inside tableView

---
+++ quiz/Qquiz/Utility.swift
@@ -0,0 +1,18 @@
+//
+//  Utility.swift
+//  Qquiz
+//
+//  Created by Fahim Rahman on 4/3/20.
+//  Copyright © 2020 Fahim Rahman. All rights reserved.
+//
+
+import UIKit
+
+struct SelectedStates {
+    
+    var rows1 = [Int]()
+    var rows2 = [Int]()
+    var buttonTags = [Int]()
+  
+    
+}
